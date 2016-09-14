4ab10437d239f1894b68032d56e6a7eb2427b025
Julian Sarrelli <julisarrelli@JuliSarrelli.local>
2016-09-14 08:19:29 -0300

    andaaa

--- app/src/main/java/com/example/julisarrelli/findmycar/MainActivity.java
+++ app/src/main/java/com/example/julisarrelli/findmycar/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 
-
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -21,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+
 import android.content.Context;
 import android.location.Location;
 import android.location.Criteria;
@@ -42,6 +42,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -55,7 +56,7 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private Location location;
 
 
     @Override
@@ -73,14 +74,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-
-
     }
 
 
     @Override
     public void onMapReady(GoogleMap map) {
-        mMap=map;
+        mMap = map;
 
 
         LatLng sydney = new LatLng(37.1833, 67.3667);
@@ -98,57 +97,28 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
 
-
+        //activamos la location
         mMap.setMyLocationEnabled(true);
-       // mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+
+        //se instancia la location
+        location=getLocation();
+
+        //centra la camara
+        if (location != null) {centrarCamara(mMap);}
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
 
 
-        //obtiene la location y centra la camara en la misma
+                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Aca dejaste el auto!").snippet("Consider yourself located"));
 
 
-    //   LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        LocationManager  locationManager= (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
-
-
-       Criteria criteria = new Criteria();
-       String provider = locationManager.getBestProvider(criteria, true);
-
-            // este anda mal, es mejor el network provider que es el unico que anda
-//        Location location = locationManager.getLastKnownLocation(provider);
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-
-        if(location!=null){
-
-            //esta forma centra la camara pero sin animacion
-//            double latitude = location.getLatitude();
-//            double longitud = location.getLongitude();
-//            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitud)));
-
-
-            //esta forma centra la camara con animacion
-            LatLng target = new LatLng(location.getLatitude(), location.getLongitude());
-        CameraPosition position = this.mMap.getCameraPosition();
-
-        CameraPosition.Builder builder = new CameraPosition.Builder();
-        builder.zoom(15);
-        builder.target(target);
-
-        this.mMap.animateCamera(CameraUpdateFactory.newCameraPosition(builder.build()));
-
-
-        }
-
-//
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitud)));
-//        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
-
-
-
+            }
+        });
 
 
 ////        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You are here!").snippet("Consider yourself located"));
@@ -158,10 +128,45 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 ////        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
 
 
+    }
 
+    public Location getLocation() {
+
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return null;
+        }
+        //return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        return locationManager.getLastKnownLocation(provider);
     }
 
 
+    public void centrarCamara(GoogleMap mMap)
+    {
+
+        //esta forma centra la camara pero sin animacion
+//            double latitude = location.getLatitude();
+//            double longitud = location.getLongitude();
+//            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitud)));
+
+
+        //esta forma centra la camara con animacion
+        LatLng target = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraPosition position = this.mMap.getCameraPosition();
+
+        CameraPosition.Builder builder = new CameraPosition.Builder();
+        builder.zoom(15);
+        builder.target(target);
+
+        this.mMap.animateCamera(CameraUpdateFactory.newCameraPosition(builder.build()));
+    }
 
 
 
